1a79757ba32bbd1a2062077dddcbd8a2e4da48a3
Jordie <jordiehendl@gmail.com>
2025-01-20 00:44:06 -0600

    start adding types

---
+++ src/api/meshi/bits/base.hpp
@@ -0,0 +1,13 @@
+#pragma once
+namespace meshi {
+  class Object {
+    public:
+    private:
+
+  };
+
+  class Component : public Object {
+    public:
+    private:
+  };
+}
---
+++ src/api/meshi/bits/renderables.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <meshi/bits/base.hpp>
+namespace meshi {
+  class Renderable : public Object {
+    public:
+
+  };
+
+
+  class Entity : public Renderable {
+    public:
+
+  };
+
+  class Actor : public Renderable {
+    public:
+
+  };
+}
