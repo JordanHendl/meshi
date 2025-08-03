11381b9ddcba9bc1d1ad46c6a1d7d3fa9bcc60b2
Jordan Hendl <jordiehendl@gmail.com>
2025-08-03 15:14:27 -0500

    Download meshi-rs library during build

--- cmake/meshi_rs.cmake
+++ cmake/meshi_rs.cmake
@@ -1,24 +1,51 @@
-set(MESHI_RS_PROJECT_DIR ${CMAKE_SOURCE_DIR}/meshi-rs)
-set(CARGO_STAMP_FILE ${CMAKE_CURRENT_BINARY_DIR}/cargo_build_stamp)
+set(MESHI_RS_VERSION 0.0.1)
+set(MESHI_RS_BASE_URL https://github.com/JordanHendl/meshi-rs/releases/download/v${MESHI_RS_VERSION})
 
-#TODO add support for pulling rust headers and targets from 
-# https://github.com/JordanHendl/meshi-rs/releases/download/v0.0.1/meshi.so
-# https://github.com/JordanHendl/meshi-rs/releases/download/v0.0.1/meshi.dll
+set(MESHI_RS_DIR ${CMAKE_BINARY_DIR}/meshi-rs)
+set(RUST_TARGET_DIR ${MESHI_RS_DIR})
 
-#Declare the imported Rust library for use in linking
+if(WIN32)
+  set(MESHI_RS_ARTIFACT meshi.dll)
+  set(MESHI_RS_LIBRARY_NAME meshi.dll)
+else()
+  set(MESHI_RS_ARTIFACT meshi.so)
+  set(MESHI_RS_LIBRARY_NAME libmeshi.so)
+endif()
+
+set(MESHI_RS_URL ${MESHI_RS_BASE_URL}/${MESHI_RS_ARTIFACT})
+set(MESHI_RS_DOWNLOAD ${MESHI_RS_DIR}/${MESHI_RS_ARTIFACT})
+set(MESHI_RS_LIBRARY_PATH ${RUST_TARGET_DIR}/${MESHI_RS_LIBRARY_NAME})
+
+add_custom_command(
+  OUTPUT ${MESHI_RS_DOWNLOAD}
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${MESHI_RS_DIR}
+  COMMAND ${CMAKE_COMMAND} -DURL=${MESHI_RS_URL} -DFILE=${MESHI_RS_DOWNLOAD}
+          -P ${CMAKE_CURRENT_LIST_DIR}/download_meshi_rs.cmake
+  COMMENT "Downloading meshi-rs from ${MESHI_RS_URL}"
+)
+
+add_custom_target(download_meshi_rs DEPENDS ${MESHI_RS_DOWNLOAD})
+
+add_custom_command(
+  OUTPUT ${MESHI_RS_LIBRARY_PATH}
+  DEPENDS ${MESHI_RS_DOWNLOAD}
+  COMMAND ${CMAKE_COMMAND} -E copy ${MESHI_RS_DOWNLOAD} ${MESHI_RS_LIBRARY_PATH}
+  COMMENT "Copying meshi-rs library to ${MESHI_RS_LIBRARY_PATH}"
+)
+
+add_custom_target(copy_meshi_library DEPENDS ${MESHI_RS_LIBRARY_PATH})
+
 add_library(meshi-rs SHARED IMPORTED)
 
 if(WIN32)
   set_target_properties(meshi-rs PROPERTIES
-    IMPORTED_LOCATION ${RUST_TARGET_DIR}/meshi.dll
+    IMPORTED_LOCATION ${MESHI_RS_LIBRARY_PATH}
     IMPORTED_IMPLIB ${RUST_TARGET_DIR}/meshi.dll.lib
   )
 else()
   set_target_properties(meshi-rs PROPERTIES
-    IMPORTED_LOCATION ${RUST_TARGET_DIR}/libmeshi.so
+    IMPORTED_LOCATION ${MESHI_RS_LIBRARY_PATH}
   )
 endif()
 
-# Ensure the copy runs before anything that uses meshi-rs
 add_dependencies(meshi-rs copy_meshi_library)
-
